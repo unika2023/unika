36a8fa97a52411c45a3cd596fc7dc0a21159c46f
Cuauhtemoc Rodriguez <1979cuau@gmail.com>
2023-03-31 11:14:03 -0600

    update VoBo file

--- includes/config/database.php
+++ includes/config/database.php
@@ -4,7 +4,7 @@ function conectarDB()
 {
     // 'unikabie_admin', 'Ivan1975*'
     //$db = mysqli_connect('localhost', 'unikabie_admin', 'Ivan1975*', 'unikabie_bienesraices');
-    $db = mysqli_connect('localhost', 'root', '', 'unikabie_bienesraices');
+    $db = mysqli_connect('localhost', 'root', 'admin', 'unikabie_bienesraices');
 
     if (!$db) {
 
